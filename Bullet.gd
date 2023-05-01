da8833a3e50ecd0479efe70dca3e0efe2a9774d0
nkp-2001 <83334420+nkp-2001@users.noreply.github.com>
2023-05-01 18:25:55 +0200

    Two Player

---
+++ Bullet.gd
@@ -0,0 +1,22 @@
+extends RigidBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+
+func _on_body_entered(body):
+	print("Hit")
+	queue_free()
+
+
+func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	print("Hit")
+	queue_free()
