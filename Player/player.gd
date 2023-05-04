a1f710c2c1e76bcb1026366f8c0193bb73266558
nkp-2001 <83334420+nkp-2001@users.noreply.github.com>
2023-05-04 14:20:36 +0200

    Two Players

--- Player/player.gd
+++ Player/player.gd
@@ -1,24 +1,25 @@
 extends CharacterBody2D
 
+@export var playerNumber = "1";
+@export var SpriteColor = Color(255, 0, 0, 1);
+
 var GRID_SIZE=16
 @onready var ray= $RayCast2D
-
 @onready var sprite= get_node("Sprite")
 
-var inputs={
-	"ui_up":Vector2.UP,
-	"ui_down":Vector2.DOWN,
-	"ui_left":Vector2.LEFT,
-	"ui_right":Vector2.RIGHT,
-}
 
-var rotate={
-	"ui_up":Vector2.UP,
-	"ui_down":Vector2.DOWN,
-	"ui_left":Vector2.LEFT,
-	"ui_right":Vector2.RIGHT,
-}
+var inputs;
+var rotate;
 
+func _ready():
+	inputs={
+	"Move_Up_Player" + playerNumber :Vector2.UP,
+	"Move_Down_Player" + playerNumber:Vector2.DOWN,
+	"Move_Left_Player" + playerNumber:Vector2.LEFT,
+	"Move_Right_Player" + playerNumber:Vector2.RIGHT,
+	}
+	rotate = inputs;
+	sprite.modulate = SpriteColor;
 
 func _unhandled_input(event):
 	for dir in inputs.keys():
